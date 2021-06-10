1c41aab886a648425acb17aa7bbe35328c2337e9
SaltyScarf <mykyta.onyshchenko@nure.ua>
2021-06-10 15:41:08 +0300

    BSTRee base implemented

---
+++ LAB 5 ADST/BSTree.cs
@@ -0,0 +1,16 @@
+﻿namespace LAB_5_ADST
+{
+    public class BSTree<T>
+    {
+        public BSTNode<T> Root { get; set; }
+
+        public int _count;
+        
+        public int Count
+        {
+            get => _count;
+        }
+        
+        
+    }
+}
