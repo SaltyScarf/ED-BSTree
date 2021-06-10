c9ec2f22e4a0e410f1ffc5bc8902c8ec369dbeb8
SaltyScarf <mykyta.onyshchenko@nure.ua>
2021-06-10 15:51:44 +0300

    BST iterative insert Missing file attached

--- LAB 5 ADST/BSTree.cs
+++ LAB 5 ADST/BSTree.cs
@@ -11,6 +11,54 @@
             get => _count;
         }
         
-        
+        public bool BST_Insert(T value, int key)
+        {
+            if (Root == null)
+            {
+                Root = new BSTNode<T>(value, key);
+            }
+            else
+            {
+                BSTNode<T> currentParent = Root;
+
+                while (true)
+                {
+                    if (key > currentParent.Key)
+                    {
+                        if (currentParent.RightChild != null)
+                        {
+                            currentParent = currentParent.RightChild;
+                        }
+                        else
+                        {
+                            currentParent.RightChild = new BSTNode<T>(value, key);
+
+                            break;
+                        }
+                    }
+                    else if (key < currentParent.Key)
+                    {
+                        if (currentParent.LeftChild != null)
+                        {
+                            currentParent = currentParent.LeftChild;
+                        }
+                        else
+                        {
+                            currentParent.LeftChild = new BSTNode<T>(value, key);
+                            
+                            break;
+                        }
+                    }
+                    else
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            _count++;
+            
+            return true;
+        }
     }
 }
---
+++ LAB 5 ADST/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace LAB_5_ADST
+{
+    class Program
+    {
+        static void Main()
+        {
+            BSTree<int> test = new BSTree<int>();
+
+            test.BST_Insert(100, 50);
+            test.BST_Insert(200, 40);
+            test.BST_Insert(300, 70);
+            test.BST_Insert(400, 60);
+            test.BST_Insert(500, 80);
+        }
+    }
+}
