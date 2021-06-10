96749dfb2e65e7fb0d188d2b6f32e95a72b7c156
SaltyScarf <mykyta.onyshchenko@nure.ua>
2021-06-10 15:29:16 +0300

    BSTNode class implemented

---
+++ LAB 5 ADST/BSTNode.cs
@@ -0,0 +1,17 @@
+﻿namespace LAB_5_ADST
+{
+    public class BSTNode<T>
+    {
+        public T Value;
+        public int Key;
+        
+        public BSTNode<T> LeftChild { get; set; }
+        public BSTNode<T> RightChild { get; set; }
+
+        public BSTNode(T value, int key)
+        {
+            Value = value;
+            Key = key;
+        }
+    }
+}
